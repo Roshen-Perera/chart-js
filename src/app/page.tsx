daeeee27cd6c358414b2924b68e63fb2a2fb772d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-07-15 10:11:44 +0530

    feat: refactor Home component to use distinct chart data and configurations for PieChartCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import { ChartConfig } from '@/components/ui/chart';
 import React from 'react'
 
 const Home = () => {
-  const chartData = [
+  const chartData1 = [
     { browser: "chrome", visitors: 80, fill: "var(--color-chrome)" },
     { browser: "safari", visitors: 20, fill: "var(--color-safari)" }
   ];
-  const chartConfig = {
+  const chartConfig1 = {
     visitors: {
       label: "Visitors",
     },
@@ -20,13 +20,30 @@ const Home = () => {
       color: "#cfcfcfff",
     },
   } satisfies ChartConfig;
+
+  const chartData2 = [
+    { browser: "chrome", visitors: 80, fill: "var(--color-chrome)" },
+    { browser: "safari", visitors: 20, fill: "var(--color-safari)" }
+  ];
+  const chartConfig2 = {
+    visitors: {
+      label: "Visitors",
+    },
+    chrome: {
+      label: "Chrome",
+      color: "#C6B200",
+    },
+    safari: {
+      label: "Safari",
+      color: "#e4dfb9ff",
+    },
+  } satisfies ChartConfig;
   return (
     <>
       <div className="flex flex-row gap-4 justify-center flex-wrap">
 
-        <PieChartCard chartData={chartData} chartConfig={chartConfig} />
-        <PieChartCard chartData={chartData} chartConfig={chartConfig} />
-        <PieChartCard chartData={chartData} chartConfig={chartConfig} />
+        <PieChartCard chartData={chartData1} chartConfig={chartConfig1} />
+        <PieChartCard chartData={chartData2} chartConfig={chartConfig2} />
       </div>
     </>
   );
